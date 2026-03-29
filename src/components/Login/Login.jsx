37abd3733e8036c8c0a39dc97427ad0e913e7282
gmnayem631 <gmnayem631@gmail.com>
2026-03-29 09:36:01 +0600

    set up firebase with sign up functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
-    <div>
-      <h2>This is login</h2>
+    <div className="card bg-base-100 w-full max-w-sm mx-auto mt-10 shrink-0 shadow-2xl">
+      <h1 className="text-3xl text-center font-bold">Login now!</h1>
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>
+          New to this website? Please{" "}
+          <Link to={"/register"} className="text-blue-400 underline">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { Link } from "react-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../components/firebase/firebase.init";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(name, email, password);
+
+    // create user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div>
-      <h2>This is register</h2>
+    <div className="card bg-base-100 w-full max-w-sm mx-auto mt-10 shrink-0 shadow-2xl">
+      <h1 className="text-3xl text-center font-bold">Create an account</h1>
+      <div className="card-body">
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <button className="btn btn-neutral mt-4">Register</button>
+        </form>
+        <p>
+          Already have an account? Please{" "}
+          <Link to={"/login"} className="text-blue-400 underline">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
